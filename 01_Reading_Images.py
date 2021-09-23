7683a1e495a78b9bbeebfefce3a1e1fce7fb4e7d
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2021-09-23 23:41:29 +0500

    Add files via upload

---
+++ 01_Reading_Images.py
@@ -0,0 +1,19 @@
+# ################################################################
+# ################ COMPUTER VISION CHAPTER 1 #####################
+
+# ##################### Reading images #####################
+
+# Importing Libraries
+import cv2
+image_path = "lambo.jpg"
+
+# Reading the image
+img = cv2.imread(image_path)
+
+# Displaying the image
+cv2.imshow("Output", img)
+
+# without delay the image can not show
+# 1000 = 1 Sec
+cv2.waitKey(8000)
+
