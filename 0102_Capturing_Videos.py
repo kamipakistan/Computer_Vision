4044700794579bc387e275448b54e8b9b4780058
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2021-09-23 23:56:44 +0500

    Add files via upload

---
+++ 0102_Capturing_Videos.py
@@ -0,0 +1,25 @@
+# ################################################################
+# ################ COMPUTER VISION CHAPTER 1 #####################
+
+# ##################### Capturing images #####################
+
+# Reading libraries
+import cv2
+
+video_path = "E:\Movies\Dolittle (2020) HDRip 1080p  HQ Lines Telugu+Tamil+Hindi+Eng[MB].mkv"
+
+# Reading the video
+capv = cv2.VideoCapture(video_path)
+
+# The Video is consist of continues images
+# so that's why we are putting loop on the images
+
+while True:
+    success, img = capv.read()
+    cv2.imshow("video", img)
+
+    # the images will continuously showing until we press q
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+
