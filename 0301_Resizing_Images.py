50716df569b5604f3a626212e640218ecf2e1df8
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2021-10-01 10:32:32 +0500

    Update 0301_Resizing_Images.py

--- 0301_Resizing_Images.py
+++ 0301_Resizing_Images.py
@@ -1,27 +1,30 @@
+# ################################################################
+# ################ COMPUTER VISION CHAPTER 3 #####################
+# ############## RESIZING AND CROPPING OF IMAGES #################
 
-# ###############################################################
-# ################ COMPUTER VISION CHAPTER 3 ####################
-# ############## RESIZING AND CROPPING OF IMAGES ################
+# ################## RESIZING / RESCALING OF IMAGE ###############
 
-# ################## RESIZING OF IMAGE ##########################
 
 # Importing library
 import cv2
 
+
+# Defining Rescaling function
+def rescalingImg(img, scale=0.75):
+    width = int(img.shape[1] * scale)
+    height = int(img.shape[0] * scale)
+    return cv2.resize(img, (width, height))
+
+
 # Reading the image
 img = cv2.imread("lambo.png")
+print(img.shape)
 
-# Resizing the Image
-imgResize = cv2.resize(img, (200, 132))
+# Calling the Resizing function
+imgResize = rescalingImg(img, 0.4)
 
 # Displaying the Images
 cv2.imshow("Original Image", img)
 cv2.imshow("Resize Image", imgResize)
 
 cv2.waitKey(0)
-
-# Displaying The Shapes of the images
-print(img.shape)
-print(imgResize.shape)
-
-
