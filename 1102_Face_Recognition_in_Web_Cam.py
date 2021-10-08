5c6f8736c264d77078a00e90fee3700d95883734
Kamran khan <76246927+kamipakistan@users.noreply.github.com>
2021-10-08 19:31:09 +0500

    Update 1102_Face_Recognition_in_Web_Cam.py

--- 1102_Face_Recognition_in_Web_Cam.py
+++ 1102_Face_Recognition_in_Web_Cam.py
@@ -2,60 +2,65 @@
 # ################################################################
 # ################ COMPUTER VISION CHAPTER 11 ####################
 # ################################################################
-# ##################### FACE RECOGNITION  ########################
+# ##################### FACE RECOGNITION #########################
 # ################################################################
 
 # Importing Necessary libraries
 import numpy as np
+import winsound
 import os
 import cv2
 import face_recognition
 
-# ############################## Step 1 ###########################
-# ######## Loading The Images and Names from its Directory ########
+# #################################################### Step 1 #########################################################
+# ######################################### Loading The Images from its Directory #####################################
+# ################################## And Getting name from the images and assigning it to list of Names ###############
 
-path = "faces"
-images = []
-classNames = []
+path = "faces"  # Path of the faces which we want to recognize
+images = []  # list to keep the images/faces
+classNames = []  # List to keep the name of the faces
+
 myList = os.listdir(path)
 print(myList)
-for cl in myList:
-    currentImg = cv2.imread(f"{path}/{cl}")
-    images.append(currentImg)
-    classNames.append(os.path.splitext(cl)[0])
+
+for face in myList:  # loof through the faces Folder and extract all the images
+    currentImg = cv2.imread(f"{path}/{face}")  # Reading the image
+    # print(f"{path}/{face}")
+    images.append(currentImg)  # Add the read image to the images list 1 by 1
+    classNames.append(os.path.splitext(face)[0])  # Add the names of the images
 
 
-# ######################## Step 2 ###########################
-# ################### Find The Encoding #####################
+# ############################################## Step 2 ###############################################
+# ######################################### Find The Encoding #########################################
 
 
 def imgEncoding(images):
-    encodeList = []
-    for img in images:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
+    encodeList = []  # List to keep the encoding of each image
+    for img in images:  # Loof Through the images list and extract image 1 by 1
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Converting the image into RGB
+        encode = face_recognition.face_encodings(img)[0]  # Find the encoding of the img
         encodeList.append(encode)
     return encodeList
 
+
 print("Encoding Started...")
-encodeListKnown = imgEncoding(images)
+encodeListKnown = imgEncoding(images)   # Calling the function and passing the images list to the function
 print("Encoding Complete")
 
+# #########################################################  Step 3  ###################################################
+# ######################################3### Find the Matches between our Encodings ####################################
 
-# #################################  Step 3  ##################################
-# #################### Find the Matches between our Encodings #################
-
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)  # Capturing the webCam
 
 while cap.isOpened():
     success, img = cap.read()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)  # Resizing the image for fast operation
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    # Finding location os all the images present in Current Frame
+    # Finding location of all the images present in Current Frame 1by 1
     facesInCurrentFrame = face_recognition.face_locations(imgS)
 
-    # Finding the encodings of all the faces present in current frame
+    # Finding the encodings of all the faces present in current frame 1 by 1
     encodesInCurrentFrame = face_recognition.face_encodings(imgS, facesInCurrentFrame)
 
     # Finding the Matches
@@ -67,8 +72,9 @@ while cap.isOpened():
         # And it will give us a list and if some face have the lowest distance
         # it will be that person
         faceDis = face_recognition.face_distance(encodeListKnown, faceEncode)
-        print(faceDis)
+        print(f"Face Distances = {faceDis}")
         matchIndex = np.argmin(faceDis)
+        print(f"Min Face Distances Index = {matchIndex}")
 
         # Creating a bounding box around the faces
         if matches[matchIndex]:
